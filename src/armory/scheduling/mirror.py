a899fa5091d56fa9fbfed78089e9277e82f4f055
David He <davidhe137@gmail.com>
2026-05-02 22:26:30 -0400

    Ruff format + fixes.

--- src/armory/scheduling/mirror.py
+++ src/armory/scheduling/mirror.py
@@ -3,6 +3,7 @@ TODO: docs
 
 action indexes vs. control steps
 """
+
 from __future__ import annotations
 from dataclasses import dataclass
 import itertools
@@ -15,53 +16,87 @@ from armory.scheduling.latency import LatencyTracker
 
 
 robot_id: TypeAlias = str
+
+
 @dataclass
 class Event:
     time: float
+
 
 @dataclass
 class ControlStep(Event):
     observation_step: int
     action_step: int | None
 
+
 # TODO: will improve speed later, focus on correctness for now
 @dataclass(frozen=True)
 class ActionChunk:
-    observation_step: int # step when observation was captured
-    arrival_time: float # step when the chunk becomes available
-    action_start_index: int # action index of the first action in the chunk
+    observation_step: int  # step when observation was captured
+    arrival_time: float  # step when the chunk becomes available
+    action_start_index: int  # action index of the first action in the chunk
     horizon: int
+
 
 class Robot:
     def __init__(self, control_hz: float, horizon: int):
         self.control_hz = control_hz
         self.horizon = horizon
 
-        self.steps: SortedList[ControlStep] = SortedList() # steps that we have collected for sure
-        self.arrived_chunks: SortedList[ActionChunk] = SortedList() # steps that we have arrived but not processed yet
+        self.steps: SortedList[ControlStep] = (
+            SortedList()
+        )  # steps that we have collected for sure
+        self.arrived_chunks: SortedList[ActionChunk] = (
+            SortedList()
+        )  # steps that we have arrived but not processed yet
         # we keep these separate so we can make best-effort estimates of when chunks will be available
-        self.pending_chunks: SortedList[ActionChunk] = SortedList() # this is separate because we add to pending chunks only after we ack from robot
+        self.pending_chunks: SortedList[ActionChunk] = (
+            SortedList()
+        )  # this is separate because we add to pending chunks only after we ack from robot
 
     def step(self, control_step: ControlStep) -> None:
         # TODO: pass more info from client and directly assert/test here
         self.steps.add(control_step)
 
     def send_response(self, response: InferResponse) -> None:
-        self.pending_chunks.add(ActionChunk(response.server_arrival_time, response.observation_step, response.action_start_index, response.horizon))
+        self.pending_chunks.add(
+            ActionChunk(
+                response.server_arrival_time,
+                response.observation_step,
+                response.action_start_index,
+                response.horizon,
+            )
+        )
 
     def receive_response(self, response: ResponseAck) -> None:
         # TODO: remove from pending chunks
-        self.arrived_chunks.add(ActionChunk(response.observation_step, response.arrival_step, response.action_start_index, response.horizon))
+        self.arrived_chunks.add(
+            ActionChunk(
+                response.observation_step,
+                response.arrival_step,
+                response.action_start_index,
+                response.horizon,
+            )
+        )
 
     @property
     def max_arrived_action_step(self) -> int:
-        return self.arrived_chunks[-1].action_start_index + self.arrived_chunks[-1].horizon - 1
-    
+        return (
+            self.arrived_chunks[-1].action_start_index
+            + self.arrived_chunks[-1].horizon
+            - 1
+        )
+
     @property
     def max_overall_action_step(self) -> int:
         max_overall_action_step = self.max_arrived_action_step
         if self.pending_chunks:
-            max_overall_action_step = max(max_overall_action_step, self.pending_chunks[-1].action_start_index + self.pending_chunks[-1].horizon - 1)
+            max_overall_action_step = max(
+                max_overall_action_step,
+                self.pending_chunks[-1].action_start_index
+                + self.pending_chunks[-1].horizon
+                - 1,
+            )
         return max_overall_action_step
 
     def get_latest_control_step_before(self, time: float) -> ControlStep | None:
@@ -74,12 +109,20 @@ class Robot:
         while self.steps[-1].time < time:
             prev_step = self.steps[-1]
             next_action_step = prev_step.action_step
-            self.steps.append(ControlStep(prev_step.time + 1/self.control_hz, prev_step.observation_step + 1, next_action_step if next_action_step <= self.max_arrived_action_step else self.max_arrived_action_step()))
+            self.steps.append(
+                ControlStep(
+                    prev_step.time + 1 / self.control_hz,
+                    prev_step.observation_step + 1,
+                    next_action_step
+                    if next_action_step <= self.max_arrived_action_step
+                    else self.max_arrived_action_step(),
+                )
+            )
 
         self.steps.pop()
 
+
 class Mirror:
-    """Represents """
     def __init__(self):
         self.time: float = 0
         self.robots: dict[robot_id, Robot] = {}
@@ -87,40 +130,63 @@ class Mirror:
     def receive_request(self, request: InferRequest) -> None:
         if request.robot_id not in self.robots:
             # NOTE: for now, assume control_hz and execution_horizon are fixed for a robot's lifetime
-            self.robots[request.robot_id] = Robot(request.control_hz, request.execution_horizon)
-        self.robots[request.robot_id].step(ControlStep(request.observation_step, request.action_start_step))
+            self.robots[request.robot_id] = Robot(
+                request.control_hz, request.execution_horizon
+            )
+        self.robots[request.robot_id].step(
+            ControlStep(request.observation_step, request.action_start_step)
+        )
 
     def send_response(self, response: InferResponse) -> None:
         self.robots[response.robot_id].send_response(response)
-    
+
     def receive_response(self, response: ResponseAck) -> None:
         self.robots[response.robot_id].receive_response(response)
 
-    def get_control_steps(self, robot_ids: list[robot_id], latency_tracker: LatencyTracker) -> list[ControlStep]:
+    def get_chunks(
+        self, robot_ids: list[robot_id], latency_tracker: LatencyTracker
+    ) -> list[ActionChunk]:
         control_steps = []
         for robot_id in robot_ids:
             time = self.time - latency_tracker.observation_latency(robot_id)
-            control_steps.append(self.robots[robot_id].get_latest_control_step_before(time))
+            control_steps.append(
+                self.robots[robot_id].get_latest_control_step_before(time)
+            )
 
-        return control_steps
+        inference_latency = latency_tracker.infer_latency(len(robot_ids))
+        return [
+            ActionChunk(
+                control_step.observation_step,
+                time + inference_latency + latency_tracker.action_latency(robot_id),
+                control_step.action_step,
+                self.robots[robot_id].horizon,
+            )
+            for control_step, robot_id in zip(control_steps, robot_ids)
+        ]
 
     def fast_forward(self, time: float, chunks: list[ActionChunk]) -> None:
+        """Jumps to next time the GPU is available"""
         # process chunks
         for chunk in chunks:
             self.robots[chunk.robot_id].receive_response(chunk)
 
         # step forward in time
-        for robot in self.robots.values():
+        for robot in self.robots.valuess():
             robot.step_forward(time)
+
+        self.time = time
 
     def total_actions(self) -> int:
         return sum(robot.actions_executed for robot in self.robots.values())
 
+
 # TODO: can be modified to support different search strategies/pruning/stopping criteria
-def search(initial_mirror: Mirror, latency_tracker: LatencyTracker, horizon: float) -> list[Robot]:
+def search(
+    initial_mirror: Mirror, latency_tracker: LatencyTracker, horizon: float
+) -> list[Robot]:
     """Search through all schedules that keep the GPU busy until time + horizon"""
 
-    best_objective = -float('inf')
+    best_objective = -float("inf")
     best_schedule = []
     end_time = initial_mirror.time + horizon
     # FIXME: don't access private
@@ -133,7 +199,12 @@ def search(initial_mirror: Mirror, latency_tracker: LatencyTracker, horizon: flo
 
     def generate_candidates(mirror: Mirror) -> list[Robot]:
         # TODO: for now just return combinations
-        return itertools.chain.from_iterable([itertools.combinations(mirror.robots, i) for i in range(1, max_batch_size + 1)])
+        return itertools.chain.from_iterable(
+            [
+                itertools.combinations(mirror.robots, i)
+                for i in range(1, max_batch_size + 1)
+            ]
+        )
 
     def dfs(mirror: Mirror, schedule: list[list[robot_id]]):
         nonlocal best_objective, best_schedule
@@ -149,13 +220,13 @@ def search(initial_mirror: Mirror, latency_tracker: LatencyTracker, horizon: flo
                 continue
 
             next_state = copy.deepcopy(mirror)
-            control_steps = mirror.get_control_steps(batch, latency_tracker)
-            chunks = [ActionChunk(control_step.observation_step, , control_step.action_step, robot.horizon) for control_step, robot in zip(control_steps, batch)]
+            chunks = next_state.get_chunks(batch, latency_tracker)
             next_state.fast_forward(next_time, chunks)
             dfs(next_state, schedule + [batch])
-    
+
     dfs(initial_mirror, [])
     return best_schedule
+
 
 """
 simulating the future:
@@ -163,4 +234,3 @@ simulating the future:
     - i need this to know which chunks to queue
 - if i don't maintain any sort of curren state and calculate everything on the fly, i can just queue all events that I know
 """
-
